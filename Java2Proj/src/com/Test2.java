fa77263986895819184d39e7ab93dd8a549be3dd
krishnaiah, avvaru <krishnaiah.avvaru@hotmail.com>
2017-09-24 11:16:54 -0400

    status

---
+++ Java2Proj/src/com/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author avaru
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
