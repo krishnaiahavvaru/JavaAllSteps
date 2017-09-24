d78bedee5a4d6fbe98317814580bed6fcaf8a155
krishnaiah, avvaru <krishnaiah.avvaru@hotmail.com>
2017-09-24 11:14:05 -0400

    testproj

---
+++ Java1Proj/src/test/Test1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author avaru
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test");
+	}
+
+}
